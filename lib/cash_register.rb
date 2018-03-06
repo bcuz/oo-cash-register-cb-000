3d47005c6b52819b6ced8d1a28a15697997bceb6
adam n <adam.nawroc@gmail.com>
2018-03-06 13:20:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,8 +20,8 @@ class CashRegister
 
   def apply_discount
     # whut is discount
-    @total * (1 - (@discount / 100.0) )
-    @total -= @discount
+    @total -= @total * (1 - (@discount / 100.0) )
+    # @total -= @discount
 
   end
 end
